a83cc600944c88a8709be04a64e8977739988ebd
Shaun Baer <shaun@bluecadet.com>
2019-02-19 15:34:37 -0500

    Start v2

---
+++ cms/drupal/web/themes/custom/bluecadet_base/assets/dist/js/alt.js
@@ -0,0 +1,8 @@
+var logger = function logger(text) {
+  console.log("Log: " + text);
+};
+
+logger('Hello World');
+logger('alt.js');
+
+//# sourceMappingURL=alt.js.map
---
+++ cms/drupal/web/themes/custom/bluecadet_base/assets/dist/js/app.js
@@ -0,0 +1,8 @@
+var logger = function logger(text) {
+  console.log("Log: " + text);
+};
+
+logger('Hello World');
+logger('app.js');
+
+//# sourceMappingURL=app.js.map
